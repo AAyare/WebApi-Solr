25aa0800936005fb01dd25cae58f738bdf323b04
Sravani-Tummepalli <stummepalli@evoketechnologies.com>
2014-02-27 20:19:29 +0530

    Added sample project -Integrate Ninject

---
+++ Evoke.Rnd.Solr/Evoke.Rnd.Solr.Api/Controllers/CorporateController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Evoke.Rnd.Solr.Api.Controllers
+{
+    public class CorporateController : ApiController
+    {
+
+    }
+}
--- Evoke.Rnd.Solr/Evoke.Rnd.Solr.Api/Controllers/HomeController.cs
+++ Evoke.Rnd.Solr/Evoke.Rnd.Solr.Api/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Evoke.Rnd.Solr.Api.Models;
 
 namespace Evoke.Rnd.Solr.Api.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMessageService _messageService;
+        public HomeController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
         public ActionResult Index()
         {
+            ViewBag.Message = _messageService.GetWelcomeMessage();
             return View();
         }
     }
